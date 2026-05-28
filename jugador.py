f52682368b42b6767b37958f640d8753c4f2ccfe
da <dasd1309@gmail.com>
2026-05-28 12:35:46 -0400

    jugador arreglo de %

--- jugador.py
+++ jugador.py
@@ -3,45 +3,48 @@ from persona import Persona
 
 class Jugador(Persona):
 
-    def __init__(
-        self,
-        nombre,
-        cedula,
-        edad,
-        posicion
-    ):
+    def __init__(self, nombre, cedula, edad, posicion):
 
-        super().__init__(
-            nombre,
-            cedula,
-            edad
-        )
+        super().__init__(nombre, cedula, edad)
 
         self.__posicion = posicion
-        self.__puntos = 0
+        self.__bolas_lanzadas = 0
+        self.__bolas_acertadas = 0
 
     @property
     def posicion(self):
-
         return self.__posicion
 
     @property
-    def puntos(self):
+    def bolas_lanzadas(self):
+        return self.__bolas_lanzadas
 
-        return self.__puntos
+    @property
+    def bolas_acertadas(self):
+        return self.__bolas_acertadas
 
-    def registrar_puntos(self, puntos):
+    def registrar_estadisticas(self, lanzadas, acertadas):
 
-        self.__puntos += puntos
+        self.__bolas_lanzadas += lanzadas
+        self.__bolas_acertadas += acertadas
+
+    def calcular_porcentaje(self):
+
+        if self.__bolas_lanzadas == 0:
+            return 0
+
+        return (
+            self.__bolas_acertadas /
+            self.__bolas_lanzadas
+        ) * 100
 
     def mostrar_info(self):
 
         return (
             f"{super().mostrar_info()} | "
             f"Posición: {self.__posicion} | "
-            f"Puntos: {self.__puntos}"
+            f"Acierto: {self.calcular_porcentaje():.2f}%"
         )
 
     def __str__(self):
-
         return self.mostrar_info()
