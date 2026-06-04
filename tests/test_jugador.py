6785a50631e89a9d7f2b47663936ce9cdaba2fe4
da <dasd1309@gmail.com>
2026-06-04 01:50:14 -0400

    eliminamos el test general y preferimos hacerlo individualmente por mas comodidad

---
+++ tests/test_jugador.py
@@ -0,0 +1,30 @@
+import unittest
+
+from jugador import Jugador
+
+
+class TestJugador(unittest.TestCase):
+
+    def test_calcular_porcentaje(self):
+
+        jugador = Jugador(
+            "Juan",
+            "123",
+            20,
+            "Puntero"
+        )
+
+        jugador.registrar_estadisticas(
+            10,
+            8
+        )
+
+        self.assertEqual(
+            jugador.calcular_porcentaje(),
+            80
+        )
+
+
+if __name__ == "__main__":
+
+    unittest.main()
